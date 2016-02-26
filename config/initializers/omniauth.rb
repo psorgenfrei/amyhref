6fef4ffaf6accd852508c1a44ece5864b77307db
suttree <duncan@suttree.com>
2016-02-26 21:09:30 +0000

    working Gmail login and inbox access

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, Rails.application.secrets.client_id , Rails.application.secrets.client_secret, {
-    #scope: ['email',
-    #'https://www.googleapis.com/auth/gmail.modify'],
     scope: "email, profile, https://mail.google.com/",
     prompt: "consent",
     access_type: 'offline'
