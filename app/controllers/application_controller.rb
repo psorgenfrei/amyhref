42f4021fb3d3295202d7b3ad017733ad0a262648
suttree <duncan@suttree.com>
2016-02-27 16:05:38 +0000

    current_user tidyup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,12 @@ class ApplicationController < ActionController::Base
 
   private
   def current_user
-    session[:current_user]
+    User.where(:id => session[:current_user]).first rescue false
   end
   helper_method :current_user
+
+  def logged_in?
+    current_user.present?
+  end
+  helper_method :logged_in
 end
