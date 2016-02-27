ad114a721c61ba0b0f6a3af69f1beefec2669e13
suttree <duncan@suttree.com>
2016-02-27 21:33:29 +0000

    wip detecting newsletters

--- app/controllers/you_controller.rb
+++ app/controllers/you_controller.rb
@@ -8,5 +8,32 @@ class YouController < ApplicationController
     @inbox_messages_count = @imap.status('INBOX', ['MESSAGES'])['MESSAGES']
     @all_messages_count = @imap.status("[Google Mail]/All Mail", ['MESSAGES'])['MESSAGES']
 
+    @imap.examine("[Google Mail]/All Mail")
+    @imap.uid_search(["NOT", "DELETED"]).each_with_index do |message_id, index|
+      next if index >= 50
+      envelope = @imap.fetch(message_id, "ENVELOPE")[0].attr["ENVELOPE"]
+      "#{envelope.from[0].name}: \t#{envelope.subject}"
+    end
+
+    @messages = []
+    @imap.search(['SINCE', 1.week.ago]).each do |message_id|
+      #envelope = @imap.fetch(message_id, "ENVELOPE")[0].attr["ENVELOPE"]
+      #@messages << "#{envelope.from[0].name}: \t#{envelope.subject}"
+
+      email_header = @imap.fetch(message_id, 'RFC822.HEADER')
+
+      if email_header[0].attr['RFC822.HEADER'].downcase.include? 'list-unsubscribe'
+        envelope = @imap.fetch(message_id, "ENVELOPE")[0].attr["ENVELOPE"]
+        @messages << "#{envelope.from[0].name}: \t#{envelope.subject}"
+      end
+    end
+
+    # using Mail to parse responses
+    #body = @imap.fetch(message_id,'BODY[TEXT]')[0].attr['BODY[TEXT]']
+    #msg = @imap.fetch(-1,'RFC822')[0].attr['RFC822']
+    #mail = Mail.read_from_string msg
+
+    @imap.logout
+    @imap.disconnect
   end
 end
