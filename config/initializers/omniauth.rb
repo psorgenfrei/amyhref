163394e15760f18a4ac0c315dd8b5cf811dc2e70
suttree <duncan@suttree.com>
2016-02-27 20:17:26 +0000

    skip jwt to avoid invalid iat errors

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, Rails.application.secrets.client_id , Rails.application.secrets.client_secret, {
     scope: "email, profile, https://mail.google.com/",
     #prompt: "consent",
-    access_type: 'offline'
+    access_type: 'offline',
+    skip_jwt: true
   }
 end
