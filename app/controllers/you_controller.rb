0de5ff4743a17898a9981b70c9b13976a94a42db
suttree <duncan@suttree.com>
2016-03-02 16:21:01 +0000

    stylin'

--- app/controllers/you_controller.rb
+++ app/controllers/you_controller.rb
@@ -2,6 +2,6 @@ class YouController < ApplicationController
   before_filter :require_user
 
   def index
-    @hrefs = current_user.hrefs.where(['created_at > ? AND created_at < ?', Date.today.at_beginning_of_day, Date.today.at_end_of_day]).group(:domain).where(:good => true).order('RAND()').limit(22)
+    @hrefs = current_user.hrefs.where(['created_at > ? AND created_at < ?', Date.today.at_beginning_of_day, Date.today.at_end_of_day]).group(:newsletter_id).where(:good => true).order('id DESC')
   end
 end
