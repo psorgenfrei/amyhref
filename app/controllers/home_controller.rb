4ff2e0f9c9df90b2e9f444eac67da6f675b08f5d
suttree <duncan@suttree.com>
2016-03-05 10:27:15 +0000

    stylin'
    - new homepage with db generated links
    - removed google oauth, replaced w/ custom button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    user = User.where(email: 'amyhref@gmail.com').first || User.last
+    @hrefs = user.hrefs.where(:good => true). order('created_at DESC, rating ASC').page(params[:page])
   end
 
   def archives
