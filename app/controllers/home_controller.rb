f28c56c827b2762b32a02080ba2c174d8fb7e962
suttree <duncan@suttree.com>
2016-03-09 09:57:32 +0000

    limit home page to 20-odd stories

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def index
     user = User.where(email: 'amyhref@gmail.com').first
-    @hrefs = Href.where(user_id: user.id, good: true, good_host: true, good_path: true).group('DATE(created_at), newsletter_id').order('created_at DESC, rating ASC').page(params[:page])
+    @hrefs = Href.where(user_id: user.id, good: true, good_host: true, good_path: true).group('DATE(created_at), newsletter_id').order('created_at DESC, rating ASC').limit(22)
   end
 
-  def archives
-    render '/home/' + params[:archives] + '.html.slim', :layout => 'application'
-  end
+  #def archives
+  #  render '/home/' + params[:archives] + '.html.slim', :layout => 'application'
+  #end
 end
