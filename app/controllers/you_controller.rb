1bca1b16fefbf9d72ca6503ad950912e4738d932
suttree <duncan@suttree.com>
2016-03-14 12:10:17 +0000

    infinite scroll

--- app/controllers/you_controller.rb
+++ app/controllers/you_controller.rb
@@ -4,26 +4,44 @@ class YouController < ApplicationController
 
   def index
     @hrefs = current_user.hrefs.where(good: true). order('created_at DESC, rating ASC').paginate(:page => params[:page], :per_page => 5)
+
+    if request.xhr?
+      render :partial => 'shared/href', :collection => @hrefs
+    else
+      render
+    end
   end
 
   def newsletters
     @newsletter = Newsletter.where(id: params[:newsletter_id]).first
     @hrefs = current_user.hrefs.where(good: true, newsletter_id: @newsletter.id).order('created_at DESC, rating ASC').paginate(:page => params[:page], :per_page => 5)
 
-    render action: :index
+    if request.xhr?
+      render :partial => 'shared/href', :collection => @hrefs
+    else
+      render action: :index
+    end
   end
 
   def search
     query = '%' + params[:q].downcase + '%'
     @hrefs = current_user.hrefs.joins(:newsletter).where(good: true).where(['LOWER(hrefs.url) LIKE ? OR LOWER(newsletters.email) = ?', query, query]).order('created_at DESC, rating ASC').paginate(:page => params[:page], :per_page => 5)
 
-    render action: :index
+    if request.xhr?
+      render :partial => 'shared/href', :collection => @hrefs
+    else
+      render action: :index
+    end
   end
 
   def junk
     @hrefs = current_user.hrefs.where(good: false).order('created_at DESC, rating ASC').paginate(:page => params[:page], :per_page => 5)
 
-    render action: :index
+    if request.xhr?
+      render :partial => 'shared/href', :collection => @hrefs
+    else
+      render action: :index
+    end
   end
 
   protected
