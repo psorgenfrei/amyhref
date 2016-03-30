07f195450800719453e9fade92bfcda7d9eff39b
suttree <duncan@suttree.com>
2016-03-30 15:12:27 +0100

    Highlights

--- app/controllers/you_controller.rb
+++ app/controllers/you_controller.rb
@@ -12,6 +12,16 @@ class YouController < ApplicationController
     end
   end
 
+  def highlights
+    @hrefs = current_user.hrefs.where(good: true, good_host: true, good_path: true).order('created_at DESC, rating ASC').paginate(:page => params[:page], :per_page => 5)
+
+    if request.xhr?
+      render :partial => 'shared/href', :collection => @hrefs
+    else
+      render action: :index
+    end
+  end
+
   def newsletter
     @newsletter = Newsletter.where(id: params[:newsletter_id]).first
     @hrefs = current_user.hrefs.where(good: true, newsletter_id: @newsletter.id).order('created_at DESC, rating ASC').paginate(:page => params[:page], :per_page => 5)
