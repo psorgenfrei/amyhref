955c3ccf0545df8b94561f3faf79dd4ba62d9064
suttree <duncan@suttree.com>
2016-03-31 14:04:16 +0100

    working on highlights

--- app/controllers/you_controller.rb
+++ app/controllers/you_controller.rb
@@ -3,7 +3,7 @@ class YouController < ApplicationController
   before_filter :fetch_newsletters
 
   def index
-    @hrefs = current_user.hrefs.where(good: true). order('created_at DESC, rating ASC').paginate(:page => params[:page], :per_page => 5)
+    @hrefs = current_user.hrefs.where(good: true).order('created_at DESC, rating ASC').paginate(:page => params[:page], :per_page => 5)
 
     if request.xhr?
       render :partial => 'shared/hrefs'
@@ -13,7 +13,7 @@ class YouController < ApplicationController
   end
 
   def highlights
-    @hrefs = current_user.hrefs.where(good: true).where('rating > -20').order('created_at DESC, rating ASC').paginate(:page => params[:page], :per_page => 5)
+    @hrefs = current_user.hrefs.where(good: true).where('rating > -20').group('DATE(created_at), newsletter_id').order('created_at DESC, rating ASC').paginate(:page => params[:page], :per_page => 5)
 
     if request.xhr?
       render :partial => 'shared/href', :collection => @hrefs
