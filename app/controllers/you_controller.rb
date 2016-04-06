f7ab9fee408f28745e59d1019c4c0dc237da0c86
Duncan Gough <duncan@suttree.com>
2016-04-06 10:04:25 +0100

    less restrictive highlights

--- app/controllers/you_controller.rb
+++ app/controllers/you_controller.rb
@@ -13,7 +13,7 @@ class YouController < ApplicationController
   end
 
   def highlights
-    @hrefs = current_user.hrefs.where(good: true).where('rating > -20').group('DATE(created_at), newsletter_id').order('created_at DESC, rating ASC').paginate(:page => params[:page], :per_page => 5)
+    @hrefs = current_user.hrefs.where(good: true).where('rating > -25').group('DATE(created_at), newsletter_id').order('created_at DESC, rating ASC').paginate(:page => params[:page], :per_page => 5)
 
     if request.xhr?
       render :partial => 'shared/href', :collection => @hrefs
