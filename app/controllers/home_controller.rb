4ba70ed8a23c1f73f29fdafc9b0f7b108df6940c
suttree <duncan@suttree.com>
2016-04-07 16:00:50 +0100

    tweak the homepage content
    - favour higher rated items

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     user = User.where(email: 'amyhref@gmail.com').first
-    @hrefs = Href.where(user_id: user.id, good: true, good_host: true, good_path: true).group('DATE(created_at), newsletter_id').order('created_at DESC, rating ASC').limit(12)
+    @hrefs = Href.where(user_id: user.id, good: true, good_host: true, good_path: true).where('rating > -25').group('DATE(created_at), newsletter_id').order('created_at DESC, rating ASC').limit(12)
   end
 end
